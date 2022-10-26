2f4e31bf2feda9d5e0e32c64a7a60222bce5d57f
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-26 11:10:56 -0500

    Create foreing key for user_id in posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_233613) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_155059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_233613) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "posts_users", id: false, force: :cascade do |t|
@@ -62,4 +64,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_233613) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
