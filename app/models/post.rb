ff0817fba3cdf4ee14534f380c0b44531d684993
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-29 19:57:39 -0500

    Add validations for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   after_create :update_post_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
+
   def five_most_recent_comments
     comments.last(5)
   end
