4a07650e4c35b09d8752113fab8a01efa873fdb5
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-31 14:32:18 -0500

    Correct rubocop offenses

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
 class PostsController < ApplicationController
-  
   def index
     @user = User.find(params[:author_id])
-    @posts = Post.where(author_id: params[:author_id]) 
+    @posts = Post.where(author_id: params[:author_id])
   end
 
   def show
