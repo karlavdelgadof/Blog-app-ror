d4b304076eff5274191b8dac0435e1b767d31c77
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-31 16:52:23 -0500

    Delete unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'users/show'
-  get 'users/index'
   root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
