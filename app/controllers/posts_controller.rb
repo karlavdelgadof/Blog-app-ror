799a2e6d21ba3fc4e262236e754a2dbd7d694005
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-31 14:22:37 -0500

    Generate controller and view for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  
+  def index
+    @user = User.find(params[:author_id])
+    @posts = Post.where(author_id: params[:author_id]) 
+  end
+
+  def show
+    @user = User.find(params[:author_id])
+    @post = Post.find(params[:id])
+  end
+end
