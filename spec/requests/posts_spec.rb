cb642536f5fa8591860290a3ce0aec59ea283af8
karlavdelgadof <karlavdelgadof@gmail.com>
2022-11-01 18:30:48 -0500

    Correct rubocop offenses

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  before(:each) do 
+  before(:each) do
     @user = User.create(name: 'Mumenya', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Kenya.')
     3.times do
       Post.create(author: @user, title: 'Title', text: 'This is my post')
@@ -9,14 +9,14 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'GET #index' do
-    before(:example) {get user_posts_path(@user.id)}
+    before(:example) { get user_posts_path(@user.id) }
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
     it 'renders correct template' do
-      expect(response).to render_template(:index) 
+      expect(response).to render_template(:index)
     end
 
     it 'includes placeholder text' do
@@ -26,21 +26,20 @@ RSpec.describe 'Posts', type: :request do
 
   describe 'GET #show' do
     before(:example) do
-      @post = @user.posts[0] 
+      @post = @user.posts[0]
       get user_post_path(@user.id, @post.id)
     end
 
     it 'returns http success' do
-      expect(response).to have_http_status(:success)  
+      expect(response).to have_http_status(:success)
     end
 
     it 'renders correct template' do
-      expect(response).to render_template(:show) 
+      expect(response).to render_template(:show)
     end
 
     it 'includes placeholder text' do
       expect(response.body).to include(@post.title)
     end
-
   end
 end
