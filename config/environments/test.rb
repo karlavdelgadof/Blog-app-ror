32a7b7f77aa12c366f44432020c806efc8af8a60
karlavdelgadof <karlavdelgadof@gmail.com>
2022-11-09 12:20:04 -0500

    Require bullet gem

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require 'bullet' 
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
